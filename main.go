0dd0e771c6713aa36283ec2efc120dc3a3ca7b08
SUHROB <SRahmonov>
2020-01-05 17:10:53 +0500

    initialy version

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-
 func distance(volumeFuel float32, expenseFuel float32 )float32 {
 	carCanRideFor := 100 * expenseFuel / volumeFuel
 	return carCanRideFor
--- main_test.go
+++ main_test.go
@@ -4,19 +4,19 @@ import "testing"
 
 func Test_distance(t *testing.T) {
 	tests := []struct {
-		name string
+		name        string
 		volumeFuel  float32
 		expenseFuel float32
-		want float32
+		want        float32
 	}{
 		// TODO: Add test cases.
-		{"Mercedes-Benz", 8.5, 7.5,88.23529 },
-		{"Audi-Q7", 9.8, 8.2,83.67347},
-		{"BMW-X7",9.2, 8.6,93.47827},
+		{"Mercedes-Benz", 8.5, 7.5, 88.23529},
+		{"Audi-Q7", 9.8, 8.2, 83.67347},
+		{"BMW-X7", 9.2, 8.6, 93.47827},
 	}
 	for _, test := range tests {
 		got := distance(test.volumeFuel, test.expenseFuel)
-		if got !=test.want{
+		if got != test.want {
 			t.Error("Test fail:", test.name, "got:", got, "want:", test.want)
 		}
 
