bd7bc49249e3a098d6bc080738421755a56945c0
SRahmonov <59470717+SRahmonov@users.noreply.github.com>
2020-01-07 17:31:23 +0500

    Update main.go

--- main.go
+++ main.go
@@ -3,9 +3,6 @@ package main
 func main() {
 
 }
-	//volumeFuel -> объем топливо
-	//expenseFuel -> расход топливо
-	//distance -> расстояние
 
 func distance(volumeFuel float32, expenseFuel float32 )float32{
 		carCanRideFor := 100 * expenseFuel / volumeFuel
